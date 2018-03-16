6a885ecc3f65018ec7872ede615f281705c2223e
Volker Schröder <volker.schroeder@accenture.com>
2018-03-16 15:24:56 +0100

    open alert popups at random position around the center of the screen

--- src/de/joieVS/dso/timer/UIAlert.java
+++ src/de/joieVS/dso/timer/UIAlert.java
@@ -1,8 +1,13 @@
 package de.joieVS.dso.timer;
 
+import static java.lang.Math.random;
 import static java.lang.System.out;
 
 import java.awt.Container;
+import java.awt.GraphicsConfiguration;
+import java.awt.Insets;
+import java.awt.Rectangle;
+import java.awt.Window;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -12,7 +17,6 @@ import javax.swing.SwingUtilities;
 import javax.swing.WindowConstants;
 
 public class UIAlert {
-
 	public static void alert(final Alert alert) {
 		if (!AlertUpdater.updateActive(alert)) {
 			SwingUtilities.invokeLater(() -> {
@@ -32,9 +36,33 @@ public class UIAlert {
 				dialog.pack();
 				dialog.setVisible(true);
 				dialog.toFront();
-				dialog.setLocation(400, 300);
+				setLocation(dialog);
 				out.println("Dialog erzeugt für Ereignis " + alert);
 			});
 		}
+	}
+
+	/**
+	 * @param dialog
+	 */
+	private static void setLocation(final JFrame dialog) {
+		final Rectangle dialogBounds = dialog.getBounds();
+		final Rectangle wa = getScreenWorkingArea(dialog);
+		final double minX = wa.getX() + wa.getWidth() * 0.25d;
+		final double maxDeltaX = wa.getWidth() * 0.5d - dialogBounds.getWidth();
+		final double minY = wa.getY() + wa.getHeight() * 0.25d;
+		final double maxDeltaY = wa.getHeight() * 0.5d - dialogBounds.getHeight();
+		dialog.setLocation((int) (minX + random() * maxDeltaX), (int) (minY + random() * maxDeltaY));
+	}
+
+	private static Rectangle getScreenWorkingArea(final Window dialog) {
+		final GraphicsConfiguration gc = dialog.getGraphicsConfiguration();
+		final Insets insets = dialog.getToolkit().getScreenInsets(gc);
+		final Rectangle bounds = gc.getBounds();
+		bounds.x += insets.left;
+		bounds.y += insets.top;
+		bounds.width -= insets.left + insets.right;
+		bounds.height -= insets.top + insets.bottom;
+		return bounds;
 	}
 }
